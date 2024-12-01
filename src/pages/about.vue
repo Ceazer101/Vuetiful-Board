b6f45a28da990783f75a6745429a94648f3a4a3e
chris <chri402s@stud.kea.dk>
2024-12-01 21:32:42 +0100

    added about page

---
+++ src/pages/about.vue
@@ -0,0 +1,10 @@
+<template> 
+    <div class="about pa-6">
+        <h1>About Vuetiful Board</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus similique autem velit itaque explicabo harum aperiam impedit, eius aut facilis, vero iusto atque ipsa, rerum omnis voluptatem voluptas sint!</p>
+        <br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus similique autem velit itaque explicabo harum aperiam impedit, eius aut facilis, vero iusto atque ipsa, rerum omnis voluptatem voluptas sint!</p>
+        <br>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus similique autem velit itaque explicabo harum aperiam impedit, eius aut facilis, vero iusto atque ipsa, rerum omnis voluptatem voluptas sint!</p>
+    </div>
+</template>
