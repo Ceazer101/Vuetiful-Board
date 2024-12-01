42be5c23af60594477d51e7d6bf242ddbd33559d
chris <chri402s@stud.kea.dk>
2024-12-01 22:46:36 +0100

    added description to tasks

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -1,7 +1,11 @@
 <template>
     <v-container class="background-container">
       <v-row>
-        <v-col v-for="(column, index) in columns" :key="index" class="kanban-column">
+        <v-col
+          v-for="(column, index) in columns"
+          :key="index"
+          class="kanban-column"
+        >
           <v-card class="kanban-card">
             <v-card-title>{{ column.title }}</v-card-title>
             <v-divider></v-divider>
@@ -15,7 +19,10 @@
                 @dragover.prevent
                 @drop="onDrop(column)"
               >
-                <v-list-item-content>{{ task }}</v-list-item-content>
+                <v-list-item-content>
+                  <v-list-item-title>{{ task.title }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ task.description }}</v-list-item-subtitle>
+                </v-list-item-content>
               </v-list-item>
             </v-list>
           </v-card>
@@ -25,8 +32,8 @@
   </template>
   
   <script lang="ts">
-    import { defineComponent } from 'vue';
-    import '../styles/KanbanBoard.scss';
+  import { defineComponent } from 'vue';
+  import '../styles/KanbanBoard.scss';
   
   export default defineComponent({
     name: 'KanbanBoard',
@@ -35,36 +42,40 @@
         columns: [
           {
             title: 'To Do',
-            tasks: ['Task 1', 'Task 2', 'Task 3'],
+            tasks: [
+              { title: 'Task 1', description: 'This is the first task.' },
+              { title: 'Task 2', description: 'This is the second task.' },
+              { title: 'Task 3', description: 'This is the third task.' },
+            ],
           },
           {
             title: 'Doing',
-            tasks: ['Task 4'],
+            tasks: [{ title: 'Task 4', description: 'This task is in progress.' }],
           },
           {
             title: 'Done',
-            tasks: ['Task 5'],
+            tasks: [{ title: 'Task 5', description: 'This task is completed.' }],
           },
         ],
-        draggedTask: null as string | null,
-        draggedFrom: null as { title: string; tasks: string[] } | null,
+        draggedTask: null as { title: string; description: string } | null,
+        draggedFrom: null as { title: string; tasks: { title: string; description: string }[] } | null,
       };
     },
     methods: {
-      onDragStart(task: string, column: { title: string; tasks: string[] }) {
+      onDragStart(task: { title: string; description: string }, column: { title: string; tasks: { title: string; description: string }[] }) {
         this.draggedTask = task;
         this.draggedFrom = column;
       },
-      onDrop(column: { title: string; tasks: string[] }) {
+      onDrop(column: { title: string; tasks: { title: string; description: string }[] }) {
         if (this.draggedTask && this.draggedFrom) {
-          const taskIndex = this.draggedFrom.tasks.indexOf(this.draggedTask);
-          if (taskIndex !== -1) this.draggedFrom.tasks.splice(taskIndex, 1);
-          column.tasks.push(this.draggedTask);
-          this.draggedTask = null;
-          this.draggedFrom = null;
+            const taskIndex = this.draggedFrom.tasks.indexOf(this.draggedTask);
+            if (taskIndex !== -1) this.draggedFrom.tasks.splice(taskIndex, 1);
+            column.tasks.push(this.draggedTask);
+            this.draggedTask = null;
+            this.draggedFrom = null;
         }
-      },
-    },
+      }
+    }
   });
   </script>
   
